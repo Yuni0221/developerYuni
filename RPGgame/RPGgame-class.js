65d4f98ef5f49b1b8f1e7c3e02fea355e484d61a
Yuni0221 <moonrise002002@gmail.com>
2022-09-21 13:46:26 +0900

    new update

--- RPGgame/RPGgame-class.js
+++ RPGgame/RPGgame-class.js
@@ -17,9 +17,9 @@ class Game {
         this.monster = null;
         this.hero = null;
         this.monsterList = [
-            { name: '밍키', hp: 25, att: 10, xp: 10 },
-            { name: '새초미', hp: 50, att: 15, xp: 20},
-            { name: '희동이', hp: 150, att: 35, xp: 50},
+            { name: '"밍키"', hp: 25, att: 10, xp: 10 },
+            { name: '"새초미"', hp: 50, att: 15, xp: 20},
+            { name: '"희동이"', hp: 150, att: 35, xp: 50},
         ];
         this.start(name);
     }
@@ -63,7 +63,12 @@ class Game {
             this.updateMonsterStat();
             this.showMessage(`몬스터와 마주쳤다. ${this.monster.name}인 것 같다!`);
         } else if (input === '2') { //휴식
+            this.hero.hp = this.hero.maxHp;
+            this.updateHeroStat();
+            this.showMessage('충분한 휴식을 취했다.');
         } else if (input === '3') { //종료
+            this.showMessage('  ');
+            this.quit();
         }
     };
 
@@ -74,11 +79,32 @@ class Game {
             const { hero, monster } = this;
             hero.attack(monster);
             monster.attack(hero);
+
+            if (hero.hp <= 0) {
+                this.showMessage(`${hero.lev} 레벨에서 전사. 새 주인공을 생성하세요.`);
+                this.quit();
+            } else if (monster.hp <= 0) {
+                this.showMessage(`몬스터를 잡아 ${monster.xp} 경험치를 얻었다.`);
+                hero.getXp(monster.xp);
+                this.monster = null;
+                this.changeScreen('game');
+            } else {
             this.showMessage(`${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`);
+            }
+
             this.updateHeroStat();
             this.updateMonsterStat();
         } else if (input === '2') { //회복
+            const { hero, monster } = this;
+            hero.hp = Math.min(hero.maxHp, hero.hp + 20);
+            monster.attack(hero);
+            this.showMessage('체력을 조금 회복했다!');
+            this.updateHeroStat();
         } else if (input === '3') { //도망
+            this.changeScreen('game');
+            this.showMessage('부리나케 도망쳤다!');
+            this.monster = null;
+            this.updateMonsterStat();
         }
     }
     
@@ -99,6 +125,32 @@ class Game {
         $heroAtt.textContent = `ATT: ${hero.att}`;
     }
     
+    updateMonsterStat() {
+        const { monster } = this;
+        if (monster === null) {
+            $monsterName.textContent = '';
+            $monsterHp.textContent = '';
+            $monsterAtt.textContent = '';
+            return;
+        }
+        $monsterName.textContent = monster.name;
+        $monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
+        $monsterAtt.textContent = `ATT: ${monster.att}`;
+    }
+    showMessage(text) {
+        $message.textContent = text;
+    }
+    quit () {
+        this.hero = null;
+        this.monster = null;
+        this.updateHeroStat();
+        this.updateMonsterStat();
+        $gameMenu.removeEventListener('submit', this.onGameMenuInput);
+        $battleMenu.removeEventListener('submit', this.onBattleMenuInput);
+        this.changeScreen('start');
+        game = null;
+    }
+
 };
 
 class Hero {
@@ -118,6 +170,17 @@ class Hero {
         this.hp += 20;
         this.hp -= monster.att;
     } 
+    getXp(xp) {
+        this.xp += xp;
+        if (this.xp >= this.lev * 15) {
+            this.xp -= this.lev *15;
+            this.lev += 1;
+            this.maxHp += 5;
+            this.att += 5;
+            this.hp = this.maxHp;
+            this.game.showMessage(`레벨업! 레벨 ${this.lev}`);
+        }
+    }
 }
 
 
