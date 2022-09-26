f9fc8472e2f0f28cea557e4fa6e72fc856a28296
Yuni0221 <moonrise002002@gmail.com>
2022-09-26 15:43:35 +0900

    new update

--- Minesweeper/Minesweeper.js
+++ Minesweeper/Minesweeper.js
@@ -44,7 +44,92 @@ function plantMine() {
 }
 
 function onRightClick(event) {
+    event.preventDefault();
+    const target = event.target;
+    const rowIndex = target.parentNode.rowIndex;
+    const cellIndex = target.cellIndex;
+    const cellData = data[rowIndex][cellIndex];
+    if (cellData === CODE.MINE) { //물음표 지뢰로
+        data[rowIndex][cellIndex] = CODE.QUESTION_MINE;
+        target.className = 'question';
+        target.textContent = '?';
+    } else if (cellData === CODE.QUESTION_MINE) { //물음표 지뢰면
+        data[rowIndex][cellIndex] = CODE.FLAG_MINE; //깃발 지뢰로
+        target.className = 'flag';
+        target.textContent = '!';
+    } else if (cellData === CODE.FLAG_MINE) { //깃발 지뢰면
+        data[rowIndex][cellIndex] = CODE.MINE; //지뢰로
+        target.className = '';
+        target.textContent = 'X';
+    } else if (cellData === CODE.NORMAL) { //닫힌 칸이면
+        data[rowIndex][cellIndex] = CODE.QUESTION; // 물음표로
+        target.className = 'question';
+        target.textContent = '?';
+    } else if (cellData === CODE.QUESTION) { // 물음표면
+        data[rowIndex][cellIndex] = CODE.FLAG; // 깃발로
+        target.className = 'flag';
+        target.textContent = '!';
+    } else if (cellData === CODE.FLAG) { //깃발이면
+        data[rowIndex][cellIndex] = CODE.FLAG //닫힌 칸으로
+        target.className = '';
+        target.textContent = '';
+    }
 
+}
+
+function countMine(rowIndex, cellIndex) {
+    const mines = [CODE.MINE, CODE.QUESTION_MINE, CODE.FLAG_MINE];
+    let i = 0;
+    mines.includes(data[rowIndex - 1]?.[cellIndex - 1]) && i++;
+    mines.includes(data[rowIndex - 1]?.[cellIndex]) && i++;  
+    mines.includes(data[rowIndex - 1]?.[cellIndex + 1]) && i++;    
+    mines.includes(data[rowIndex][cellIndex - 1]) && i++;   
+    mines.includes(data[rowIndex][cellIndex + 1]) && i++; 
+    mines.includes(data[rowIndex + 1]?.[cellIndex - 1]) && i++;    
+    mines.includes(data[rowIndex + 1]?.[cellIndex]) && i++;    
+    mines.includes(data[rowIndex + 1]?.[cellIndex + 1]) && i++;    
+    return i;
+}
+
+function open(rowIndex, cellIndex) {
+    const target = $tbody.children[rowIndex]?.children[cellIndex];
+    if (!target) {
+        return;
+    }
+    const count = countMine(rowIndex, cellIndex);
+    target.textContent = count || '';
+    target.className = 'opened';
+    data[rowIndex][cellIndex] = count;
+    return count;
+}
+
+function openAround(rI, cI) {
+    const count = open(rI, cI);
+    if (count === 0) {
+        openAround(rI - 1, cI - 1);
+        openAround(rI - 1, cI);
+        openAround(rI - 1, cI + 1);
+        openAround(rI, cI - 1);
+        openAround(rI, cI + 1);
+        openAround(rI + 1, cI - 1);
+        openAround(rI + 1, cI);
+        openAround(rI + 1, cI + 1);
+    }
+}
+
+function onLeftClick(event) {
+    const target = event.target;
+    const rowIndex = target.parentNode.rowIndex;
+    const cellIndex = target.cellIndex;
+    const cellData = data[rowIndex][cellIndex];
+    if (cellData === CODE.NORMAL) {
+        openAround(rowIndex, cellIndex);
+    } else if (cellData === CODE.MINE) {     
+        target.textContent = '펑';
+        target.className = 'opened';
+        $tbody.removeEventListener('contextmenu', onRightClick);
+        $tbody.removeEventListener('click', onLeftClick);  
+    }
 }
 
 function drawTable() {
@@ -59,6 +144,8 @@ function drawTable() {
             $tr.append($td);
         }); 
         $tbody.append($tr);
+        $tbody.addEventListener('contextmenu', onRightClick);
+        $tbody.addEventListener('click', onLeftClick);
     });
 } 
 
