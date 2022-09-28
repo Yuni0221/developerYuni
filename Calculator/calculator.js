62dada1129409dc435b5cb94193c7b3b7414b6b3
Yuni0221 <moonrise002002@gmail.com>
2022-09-28 17:47:00 +0900

    new update

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -3,14 +3,22 @@ let operator = '';
 let numTwo = '';
 const $operator = document.querySelector('#operator');
 const $result = document.querySelector('#result');
-const onClickNumber = (number) => {
-    if (operator) {
-        numTwo += number;
-    } else {
-        numOne += number;
+const onClickNumber = (event) => {
+    if (!operator) {
+        numOne += event.target.textContent;
+        $result.value += event.target.textContent;
+        return;
     }
-    $result.value += number;
+
+    if (!numTwo) {
+        $result.value = '';
+    }
+    numTwo += event.target.textContent;
+    $result.value += event.target.textContent;
 };
+   
+ // 고차함수 (high order function)
+
 document.querySelector('#num-0').addEventListener('click', (onClickNumber('0')));
 document.querySelector('#num-1').addEventListener('click', (onClickNumber('1')));
 document.querySelector('#num-2').addEventListener('click', (onClickNumber('2')));
@@ -21,10 +29,19 @@ document.querySelector('#num-6').addEventListener('click', (onClickNumber('6')))
 document.querySelector('#num-7').addEventListener('click', (onClickNumber('7')));
 document.querySelector('#num-8').addEventListener('click', (onClickNumber('8')));
 document.querySelector('#num-9').addEventListener('click', (onClickNumber('9')));
-document.querySelector('#plus').addEventListener('click', () => {});
-document.querySelector('#minus').addEventListener('click', () => {});
-document.querySelector('#divide').addEventListener('click', () => {});
-document.querySelector('#multiply').addEventListener('click', () => {});
+
+const onClickOperator = (op) => () => {
+    if (numOne) {
+        operator = op;
+        $operator.value = op;
+    } else {
+        alert('숫자를 먼저 입력하세요.');
+    }
+}
+document.querySelector('#plus').addEventListener('click', onClickOperator('+'));
+document.querySelector('#minus').addEventListener('click', onClickOperator('-'))
+document.querySelector('#divide').addEventListener('click', onClickOperator('/'))
+document.querySelector('#multiply').addEventListener('click', onClickOperator('*'))
 document.querySelector('#calculate').addEventListener('click', () => {});
 document.querySelector('#clear').addEventListener('click', () => {});
 
