37a775257f4894f50f809355201a508f1f9b48f5
Yuni0221 <moonrise002002@gmail.com>
2022-10-05 11:23:35 +0900

    new update

--- RSP/RSP.js
+++ RSP/RSP.js
@@ -7,8 +7,33 @@ const IMG_URL = './rsp.png'
 $computer.style.background = `url(${IMG_URL}) 0 0`;
 $computer.style.backgroundSize = 'auto 200px';
 
-const rxpX = {
-    scissors: '0',
-    rock: '-220px',
-    paper: '-440px',
+const rspX = {
+    scissors: '0', //가위
+    rock: '-220px', //바위
+    paper: '-440px', //보
 };
+
+let computerChoice = 'scissors';
+const changeComputerHand = () => {
+    if (computerChoice === 'scissors'){
+        computerChoice = 'rock';
+    } else if (computerChoice === 'rock') {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+    $computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
+    $computer.style.backgroundSize = 'auto 200px';
+    setTimeout(changeComputerHand, 50); 
+}
+setTimeout(changeComputerHand, 50);
+
+const clickButton = () => {
+    clearInterval(intervalId);
+    setTimeout(() => {
+        intervalId = setInterval(changeComputerHand, 50);
+    }, 1000);
+};
+$rock.addEventListener('click', clickButton);
+$scissors.addEventListener('click', clickButton);
+$paper.addEventListener('click', clickButton);
