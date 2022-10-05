bf7c2feea18600286fb642fd19471f51d862acdb
Yuni0221 <moonrise002002@gmail.com>
2022-10-05 14:56:58 +0900

    new update

--- RSP/RSP.js
+++ RSP/RSP.js
@@ -24,16 +24,48 @@ const changeComputerHand = () => {
     }
     $computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
     $computer.style.backgroundSize = 'auto 200px';
-    setTimeout(changeComputerHand, 50); 
 }
-setTimeout(changeComputerHand, 50);
+let intervalId = setInterval(changeComputerHand, 50);
 
-const clickButton = () => {
-    clearInterval(intervalId);
-    setTimeout(() => {
-        intervalId = setInterval(changeComputerHand, 50);
-    }, 1000);
+const scoreTable = {
+    rock: 0,
+    scissors: 1,
+    paper: -1,
 };
+
+let clickable = true;
+let score = 0;
+const clickButton = () => {
+    if (clickable) {
+        clearInterval(intervalId);
+        clickable = false;
+        const myChoice = event.target.textContent === 'Rock'
+        ? 'rock'
+        : event.target.textContent === 'Scissors'
+         ? 'scissors'
+         : 'paper';
+    const myScore = scoreTable[myChoice];
+    const computerScore = scoreTable[computerChoice];
+    const diff = myScore - computerScore;  
+
+    let message;
+    if ([2, -1].includes(diff)) {
+        score += 1;
+        message = 'win';
+    } else if ([2, -1].includes(diff)) {
+        score -= 1;
+        message = 'Lose';
+    } else {
+        message = 'Draw';
+    }
+    $score.textContent = `${message} Total : ${score} point`;
+        setTimeout(() => {
+            clickable = true; 
+            intervalId = setInterval(changeComputerHand, 50);
+        }, 1000);
+    }
+}
+
 $rock.addEventListener('click', clickButton);
 $scissors.addEventListener('click', clickButton);
 $paper.addEventListener('click', clickButton);
