74a745da361ff1ae5a4885e8514b1db6b4b9c931
Yuni0221 <moonrise002002@gmail.com>
2022-10-07 16:26:40 +0900

    new update

---
+++ 10000hours/10000hours.js
@@ -0,0 +1,4 @@
+shareButton.addEventListener('click', copyUrl)
+openButton.addEventListener('click', openModal)
+closeButton.addEventListener('click', closeModal)
+startButton.addEventListener('click', calculator)
