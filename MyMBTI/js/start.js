77a6657fb50895bb789c3a8b31cada223e992b31
Yuni0221 <moonrise002002@gmail.com>
2022-10-13 18:39:26 +0900

    new update

--- MyMBTI/js/start.js
+++ MyMBTI/js/start.js
@@ -1,10 +1,16 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const endPoint = 12;
 
 function addAnswer(answerText, qIdx){
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
     answer.classList.add('answerList');
+    answer.classList.add('my-3');
+    answer.classList.add('py-3');
+    answer.classList.add('mx-auto');
+    answer.classList.add('fadeIn');
+
     a.appendChild(answer);
     answer.innerHTML = answerText;
 
@@ -12,9 +18,15 @@ function addAnswer(answerText, qIdx){
         var children = document.querySelectorAll('.answerList');
         for(let i = 0; i < children.length; i++){
             children[i].disabled = true;
-            children[i].style.display = 'none';
+            children[i].style.webkitAnimation = "fadeOut 0.5s";
+            children[i].style.animation = "fadeOut 0.5s";
         }
-        goNext(++qIdx);
+        setTimeout(() => {
+            for(let i = 0; i < children.length; i++){
+                children[i].style.display= 'none';
+            }
+            goNext(++qIdx);
+        }, 450)
     }, false);
 }
 
@@ -24,6 +36,8 @@ function goNext(qIdx) {
     for(let i in qnaList[qIdx].a) {
         addAnswer(qnaList[qIdx].a[i].answer, qIdx);
     }
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
 function begin(){
