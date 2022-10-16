9d57d787cf9540281c277b6345d47b0c6704d080
Yuni0221 <moonrise002002@gmail.com>
2022-10-16 12:51:11 +0900

    new update

--- MyMBTI/js/start.js
+++ MyMBTI/js/start.js
@@ -4,6 +4,46 @@ const result = document.querySelector("#result");
 const endPoint = 12;
 const select = [];
 
+function calResult(){
+    var pointArray = [
+        { name: 'mouse', value: 0, key: 0 },
+        { name: 'cow', value: 0, key: 1 },
+        { name: 'tiger', value: 0, key: 2 },
+        { name: 'rabbit', value: 0, key: 3 },
+        { name: 'dragon', value: 0, key: 4 },
+        { name: 'snake', value: 0, key: 5 },
+        { name: 'horse', value: 0, key: 6 },
+        { name: 'sheep', value: 0, key: 7 },
+        { name: 'monkey', value: 0, key: 8 },
+        { name: 'chick', value: 0, key: 9 },
+        { name: 'dog', value: 0, key: 10 },
+        { name: 'pig', value: 0, key: 11 },
+    ]
+    for (let i = 0; i < endPoint; i++){
+        var target = qnaList[i].a[select[i]];
+        for(let j = 0; j < target.length; j++) {
+            for(let k=0; k < pointArray.length; k++){
+                if(target.tyle[j] === pointArray[k].name){
+                    pointArray[k].value += 1;
+                }
+            }
+        }
+    }
+    var resultArray = pointArray.sort(function (a, b){
+        if(a.value > b.value) {
+            return -1;
+        }
+        if(a.value < b.value) {
+            return 1;
+        }
+        return 0;
+    })
+    console.log(resultArray);
+    let resultword = resultArray[0].key;
+    return resultword;
+}
+
+
 function goResult(){
     qna.style.webkitAnimation = "fadeOut 1s";
     qna.style.animation = "fadeOut 1s";
@@ -14,9 +54,11 @@ function goResult(){
             qna.style.display = "none";
             result.style.display = "block";
         }, 450)})
+        
+        calResult();
 }
 
-function addAnswer(answerText, qIdx){
+function addAnswer(answerText, qIdx, idx){
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
     answer.classList.add('answerList');
@@ -36,6 +78,7 @@ function addAnswer(answerText, qIdx){
             children[i].style.animation = "fadeOut 0.5s";
         }
         setTimeout(() => {
+            select[qIdx] = idx;
             for(let i = 0; i < children.length; i++){
                 children[i].style.display= 'none';
             }
@@ -45,14 +88,14 @@ function addAnswer(answerText, qIdx){
 }
 
 function goNext(qIdx) {
-    if(qIdx+1 === endPoint) {
+    if(qIdx === endPoint) {
         goResult();
         return;
     }
     var q = document.querySelector('.qBox');
     q.innerHTML = qnaList[qIdx].q;
     for(let i in qnaList[qIdx].a) {
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
     var status = document.querySelector('.statusBar');
     status.style.width = (100/endPoint) * (qIdx+1) + '%';
